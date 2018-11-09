18707119253adfb3a15f38d4d071b4ac26093534
Salvador Guzman <guzmansalv@gmail.com>
2018-11-08 17:25:50 -0800

    no get channel

--- src/postgres.rs
+++ src/postgres.rs
@@ -16,22 +16,3 @@ fn conn() -> postgres::Connection {
 
     return postgres::Connection::connect(params, tls).unwrap();
 }
-
-pub fn channels() -> Vec<rand::distributions::Weighted<String>> {
-    let c: postgres::Connection = conn();
-    let query: &str = "select serial, subs from youtube.entities.chan_stats where (serial, time) in (select serial, max(time) from youtube.entities.chan_stats group by serial) LIMIT 10";
-
-    let mut vec = Vec::new();
-    for row in &c.query(query, &[]).unwrap() {
-        let serial: String = row.get(0);
-        let subs: i64 = row.get(1);
-        let weight = rand::distributions::Weighted {
-            weight: subs as u32,
-            item: serial
-        };
-
-        vec.push(weight);
-    }
-
-    return vec;
-}
