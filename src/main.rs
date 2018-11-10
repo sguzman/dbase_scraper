b7d276f58b3566772fb499ab9294784156007411
Salvador Guzman <guzmansalv@gmail.com>
2018-11-09 22:58:16 -0800

    working so far

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 extern crate reqwest;
+extern crate scraper;
 
 fn main() {
     loop {
         let url: &str = "https://dbase.tube/chart/channels/subscribers/all";
 
-        let body_result  = reqwest::get(url);
-        match body_result {
-            Ok(text) => text,
-            Err(e) => {
-                println!("{:?}" , e);
-                continue;
-            }
-        }
+        let body_result: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
+        let mut body: reqwest::Response = match body_result {
+            Ok(b) => b,
+            _ => continue
+        };
+
+        println!("{:?}", body.text());
     }
 }
