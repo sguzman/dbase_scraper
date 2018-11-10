09a5ddd94fafcb8fa7aa66a0ab5c0f36396080c6
Salvador Guzman <guzmansalv@gmail.com>
2018-11-09 23:09:02 -0800

    get element

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,16 @@ fn main() {
             _ => continue
         };
 
-        println!("{:?}", body.text());
+        let text: String = match body.text() {
+            Ok(t) => t,
+            _ => continue
+        };
+
+        let document: &str = text.as_str();
+        let doc: scraper::Html = scraper::Html::parse_document(document);
+
+        let selector: scraper::Selector = scraper::Selector::parse("li.ffwd a[href]").unwrap();
+        let mut elements: scraper::html::Select = doc.select(&selector);
+        println!("{:?}", elements.next().unwrap().value().attr("href").unwrap());
     }
 }
