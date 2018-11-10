8ebce05a440b9196ac219f23dce417962b70d4c2
Salvador Guzman <guzmansalv@gmail.com>
2018-11-09 23:22:05 -0800

    got max pages

---
+++ src/http.rs
@@ -0,0 +1,30 @@
+pub fn page_count() -> u64 {
+    let url: &str = "https://dbase.tube/chart/channels/subscribers/all";
+
+    let body_result: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
+    let mut body: reqwest::Response = match body_result {
+        Ok(b) => b,
+        _ => return 0
+    };
+
+    let text: String = match body.text() {
+        Ok(t) => t,
+        _ => return 0
+    };
+
+    let document: &str = text.as_str();
+    let doc: scraper::Html = scraper::Html::parse_document(document);
+
+    let selectors: &str = "li.ffwd a[href]";
+    let selector: scraper::Selector = scraper::Selector::parse(selectors).unwrap();
+    let mut elements: scraper::html::Select = doc.select(&selector);
+    let href: &str = elements.next().unwrap().value().attr("href").unwrap();
+
+    let pat: &str = "=";
+    let begin: usize = href.rfind(pat).unwrap();
+    let n: usize = begin + 1;
+    let num: String = href.chars().skip(n).collect();
+    let real_num: u64 = num.parse::<u64>().unwrap();
+
+    return real_num;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 extern crate reqwest;
 extern crate scraper;
 
+mod http;
+
 fn main() {
     loop {
-        let url: &str = "https://dbase.tube/chart/channels/subscribers/all";
+        let n: u64 = http::page_count();
+        if n == 0 {
+            continue
+        }
 
-        let body_result: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
-        let mut body: reqwest::Response = match body_result {
-            Ok(b) => b,
-            _ => continue
-        };
-
-        let text: String = match body.text() {
-            Ok(t) => t,
-            _ => continue
-        };
-
-        let document: &str = text.as_str();
-        let doc: scraper::Html = scraper::Html::parse_document(document);
-
-        let selector: scraper::Selector = scraper::Selector::parse("li.ffwd a[href]").unwrap();
-        let mut elements: scraper::html::Select = doc.select(&selector);
-        println!("{:?}", elements.next().unwrap().value().attr("href").unwrap());
+        println!("{}", n);
     }
 }
