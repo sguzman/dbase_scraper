5305f383761edc75a9d23e0d61d653e6fa38d90b
Salvador Guzman <guzmansalv@gmail.com>
2018-11-11 14:05:02 -0800

    scrape channels

--- src/http.rs
+++ src/http.rs
@@ -1,15 +1,15 @@
-pub fn page_count() -> u64 {
+pub fn page_count() -> Option<u64> {
     let url: &str = "https://dbase.tube/chart/channels/subscribers/all";
 
     let body_result: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
     let mut body: reqwest::Response = match body_result {
         Ok(b) => b,
-        _ => return 0
+        _ => return None
     };
 
     let text: String = match body.text() {
         Ok(t) => t,
-        _ => return 0
+        _ => return None
     };
 
     let document: &str = text.as_str();
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,41 @@ mod http;
 
 fn main() {
     loop {
-        let n: u64 = http::page_count();
-        if n == 0 {
+        let n: Option<u64> = http::page_count();
+        if n.is_none() {
             continue
         }
 
-        println!("{}", n);
+        let m: u64 = n.unwrap() + 1;
+
+        for i in 1..m {
+            let url: String = if i < 2 {
+                format!("https://dbase.tube/chart/channels/subscribers/all")
+            } else {
+                format!("https://dbase.tube/chart/channels/subscribers/all?page={}", i)
+            };
+
+            let url: &str = url.as_ref();
+            let body_result: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
+            let mut body: reqwest::Response = match body_result {
+                Ok(b) => b,
+                _ => continue
+            };
+
+            let text: String = match body.text() {
+                Ok(t) => t,
+                _ => continue
+            };
+
+            let document: &str = text.as_ref();
+            let doc: scraper::Html = scraper::Html::parse_document(document);
+
+            let selectors: &str = "div.dlist_desk a[href^=\"/c/UC\"]";
+            let selector: scraper::Selector = scraper::Selector::parse(selectors).unwrap();
+
+            for element in doc.select(&selector) {
+                println!("{}", element.value().attr("href").unwrap());
+            }
+        }
     }
 }
