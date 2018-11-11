2aef4b8c8a94f423e7e8d22cf939bb05f580fab1
Salvador Guzman <guzmansalv@gmail.com>
2018-11-11 14:14:34 -0800

    http

--- src/http.rs
+++ src/http.rs
@@ -28,3 +28,38 @@ pub fn page_count() -> Option<u64> {
 
     return Some(real_num);
 }
+
+pub fn channels(i: u64) -> Vec<String> {
+    let url: String = if i < 2 {
+        format!("https://dbase.tube/chart/channels/subscribers/all")
+    } else {
+        format!("https://dbase.tube/chart/channels/subscribers/all?page={}", i)
+    };
+
+    let url: &str = url.as_ref();
+    let body_result: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
+    let mut body: reqwest::Response = match body_result {
+        Ok(b) => b,
+        _ => return Vec::new()
+    };
+
+    let text: String = match body.text() {
+        Ok(t) => t,
+        _ => return Vec::new()
+    };
+
+    let document: &str = text.as_ref();
+    let doc: scraper::Html = scraper::Html::parse_document(document);
+
+    let selectors: &str = "div.dlist_desk a[href^=\"/c/UC\"]";
+    let selector: scraper::Selector = scraper::Selector::parse(selectors).unwrap();
+
+    let mut vec: Vec<String> = Vec::new();
+    for element in doc.select(&selector) {
+        let value: &str = element.value().attr("href").unwrap();
+        let value: String = value.to_string();
+        vec.push(value);
+    }
+
+    return vec;
+}
--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,9 @@ fn main() {
         let m: u64 = n.unwrap() + 1;
 
         for i in 1..m {
-            let url: String = if i < 2 {
-                format!("https://dbase.tube/chart/channels/subscribers/all")
-            } else {
-                format!("https://dbase.tube/chart/channels/subscribers/all?page={}", i)
-            };
-
-            let url: &str = url.as_ref();
-            let body_result: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
-            let mut body: reqwest::Response = match body_result {
-                Ok(b) => b,
-                _ => continue
-            };
-
-            let text: String = match body.text() {
-                Ok(t) => t,
-                _ => continue
-            };
-
-            let document: &str = text.as_ref();
-            let doc: scraper::Html = scraper::Html::parse_document(document);
-
-            let selectors: &str = "div.dlist_desk a[href^=\"/c/UC\"]";
-            let selector: scraper::Selector = scraper::Selector::parse(selectors).unwrap();
-
-            for element in doc.select(&selector) {
-                println!("{}", element.value().attr("href").unwrap());
+            let vec: Vec<String> = http::channels(i);
+            for c in vec {
+                println!("{}", c);
             }
         }
     }
